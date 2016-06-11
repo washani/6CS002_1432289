22de242f5560e27ca5cd0f34c1fd2faf99fa991c
washani <kiaraduva@gmail.com>
2016-06-11 22:05:21 +0530

    radius variable created on top of the class and access all the places its required

--- 6CS002_1432289/src/moseley013/PictureFrame.java
+++ 6CS002_1432289/src/moseley013/PictureFrame.java
@@ -11,6 +11,8 @@ public class PictureFrame {
   public int[] reroll = null;
   public Main master = null;
 
+  int radius;
+  
   class DominoPanel extends JPanel {
     private static final long serialVersionUID = 4190229282411119364L;
 
@@ -53,7 +55,7 @@ public class PictureFrame {
     }
 
     void drawDigitGivenCentre(Graphics g, int x, int y, int diameter, int n) {
-      int radius = diameter / 2;
+      radius = diameter / 2;
       g.setColor(Color.BLACK);
       // g.drawOval(x - radius, y - radius, diameter, diameter);
       FontMetrics fm = g.getFontMetrics();
@@ -64,7 +66,7 @@ public class PictureFrame {
 
     void drawDigitGivenCentre(Graphics g, int x, int y, int diameter, int n,
         Color c) {
-      int radius = diameter / 2;
+      radius = diameter / 2;
       g.setColor(c);
       // g.drawOval(x - radius, y - radius, diameter, diameter);
       FontMetrics fm = g.getFontMetrics();
