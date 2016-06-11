20c9795a7ba7e4a2d241e12ab04b66ae0c98a7a5
washani <kiaraduva@gmail.com>
2016-06-11 21:53:52 +0530

    Created

---
+++ 6CS002_1432289/src/moseley013/Bury.java
@@ -0,0 +1,14 @@
+package moseley013;
+public class Bury {
+  static transient String[] bat = {
+    "Wickedness is a myth invented by good people to account for the curious attraction of others.","Oscar Wilde","",
+    "The flowers anew, returning seasons bring! But beauty faded has no second spring.","Frank Moore Colby","",
+    "One of the striking differences between a cat and a lie is that a cat has only nine lives.","Mark Twain","",
+    "An unexciting truth may be eclipsed by a thrilling lie.","Aldous Huxley","",
+    "Truth is generally the best vindication against slander.","Abraham Lincoln","",
+    "Though I am not naturally honest, I am sometimes by chance.","William Shakespeare","",
+    "Fallacies do not cease to be fallacies because they become fashions.","GK Chesterton","",
+    "There are a lot of lies going around...and half of them are true.","Winston Churchill","",
+    "Of all the animals, man is the only one that lies.","Mark Twain","",
+    "If you start to think about your physical or moral condition, you usually find that you are sick.","Johann Wolfgang von Goethe",""};
+}
---
+++ 6CS002_1432289/src/moseley013/IOSpecialist.java
@@ -0,0 +1,14 @@
+package moseley013;
+
+/**
+ * @author Kevan Buckley, maintained by Peter Moseley
+ * @version 2.0, 2014
+ */
+
+public class IOSpecialist {
+  public IOSpecialist() {   
+  }
+  public String getString(){
+    return IOLibrary.getString();
+  }
+}
